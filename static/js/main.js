584eaec88abe83ef89aef736679ea453f1744f10
k-koech <kelvin.koech@student.moringaschool.com>
2021-06-11 23:47:19 +0300

    validating year and month

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,37 @@
      
 function GenerateAkan() 
 {
-    if(document.getElementById("date").value<=0 || document.getElementById("date").value>31)
+    var date = document.getElementById("date").value;
+    var month = document.getElementById("month").value;
+    var year = document.getElementById("year").value;
+
+    if(date<=0 || date>31)
     {
         alert("Enter a valid date!");
     }
     else
     {
-        if(document.getElementById("month").value<1 || document.getElementById("month").value>12)
+        if(month<1 || month>12)
         {
             alert("Enter a valid month!");
         }
         else
         {
-            if(document.getElementById("year").value=="")
+            if(year=="")
             {
                 alert("Please enter the value of the year!");
+            }
+            else if(year.length<4)
+            {
+                alert("Year must have 4 digits!");
+            }
+            else
+            {
+                var gender=document.getElementsByName('gender');
+                if(!(gender[0].checked || gender[1].checked))
+                {
+                    alert("Please Select Your Gender!");
+                }
             }
 
             
