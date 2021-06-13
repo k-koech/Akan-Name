2da8fe29226d4834283072b1cfca1879f9fc4733
k-koech <kelvin.koech@student.moringaschool.com>
2021-06-13 15:08:19 +0300

    updated htnml and css

--- static/js/main.js
+++ static/js/main.js
@@ -85,3 +85,62 @@ function GenerateAkan()
     
 }
     
+
+
+!(function($) {
+    "use strict";
+  
+    // Smooth scroll for the navigation menu and links with .scrollto classes
+    var scrolltoOffset = $('#header').outerHeight() - 15;
+    $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        if (target.length) {
+          e.preventDefault();
+  
+          var scrollto = target.offset().top - scrolltoOffset;
+  
+          if ($(this).attr("href") == '#header') {
+            scrollto = 0;
+          }
+  
+          $('html, body').animate({
+            scrollTop: scrollto
+          }, 1500, 'easeInOutExpo');
+  
+          if ($(this).parents('.nav-menu, .mobile-nav').length) {
+            $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+            $(this).closest('li').addClass('active');
+          }
+  
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').fadeOut();
+          }
+          return false;
+        }
+      }
+    });
+
+    $(".portfolio-details-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+      });
+    
+      // Init AOS
+      function aos_init() {
+        AOS.init({
+          duration: 500,
+          easing: "ease-in-out",
+          once: true,
+          mirror: false
+        });
+      }
+      $(window).on('load', function() {
+        aos_init();
+      });
+    
+    })(jQuery);
